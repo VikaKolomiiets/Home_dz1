3cfd4960c6278be01ad85eea2e083ba051aca714
gena <gena.vl@gmail.com>
2023-03-25 18:30:14 +0200

    scope: Add properties

--- CrossNullApp/DataAccess/Class1.cs
+++ CrossNullApp/DataAccess/Class1.cs
@@ -2,6 +2,9 @@
 {
     public class Class1
     {
-
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
