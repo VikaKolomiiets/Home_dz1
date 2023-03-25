00bca1991d24e49542818482635e4aff0cae6a2c
Vika Kolomiets <vikakolomiets@gmail.com>
2023-03-25 18:32:42 +0200

    scope: add properties to Class1

--- CrossNullApp/DataAccess/Class1.cs
+++ CrossNullApp/DataAccess/Class1.cs
@@ -2,6 +2,11 @@
 {
     public class Class1
     {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int FullAde { get; set; }
+        public int Age { get; set;}
 
     }
 }
